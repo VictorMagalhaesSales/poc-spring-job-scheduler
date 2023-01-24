ea40cc8f536946ae165b3335d58b678e1a20c3cf
Victor Magalhães <victormagalhaessales@gmail.com>
2023-01-24 08:25:20 -0300

    Initial commit with startup project

---
+++ src/main/java/com/poc/jobscheduler/PocSpringJobSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.poc.jobscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringJobSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringJobSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/jobscheduler/PocSpringJobSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.jobscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringJobSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
