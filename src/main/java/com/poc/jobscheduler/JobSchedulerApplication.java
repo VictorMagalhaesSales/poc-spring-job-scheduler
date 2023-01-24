f5e3fab7ec404e0d1145c6a9e4966e225a0225fb
Victor Magalhães <victormagalhaessales@gmail.com>
2023-01-24 08:44:01 -0300

    Fix dependencies

--- src/main/java/com/poc/jobscheduler/JobSchedulerApplication.java
+++ src/main/java/com/poc/jobscheduler/JobSchedulerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PocSpringJobSchedulerApplication {
+public class JobSchedulerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PocSpringJobSchedulerApplication.class, args);
+		SpringApplication.run(JobSchedulerApplication.class, args);
 	}
 
 }
